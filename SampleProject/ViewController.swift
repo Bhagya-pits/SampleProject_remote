ac42e3785eebe34f6012ec1fd42782b47fc3be54
Bhagyalakshmi <bhagyalakshmi.rn@pitsolutions.com>
2022-09-01 17:55:19 +0530

    Search in Group completed

--- SampleProject/ViewController.swift
+++ SampleProject/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
         group.append(Group.init(grpName: "Group 'crash test'", typName: ["Group Chat"],member: "5 Participants"))
         group.append(Group.init(grpName: "Comment Time Test", typName: ["Group Chat"],member: "5 Participants"))
         group.append(Group.init(grpName: "Event Push Issue Test", typName: ["Time Test","Group Chat"],member: "5 Participants"))
-        
+        filteredGroup = group
         hideKeyboardWhenTappedAround()
         
     }
@@ -54,37 +54,44 @@ class ViewController: UIViewController {
     }
    
 }
-extension ViewController : UITableViewDataSource, UITableViewDelegate{
+extension ViewController : UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate{
     
     func numberOfSections(in tableView: UITableView) -> Int {
 
-        return searching ? filteredGroup.count : group.count
-        //           return group.count
+        //return searching ? filteredGroup.count : group.count
+                  return filteredGroup.count
     }
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
            let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40))
-           view.backgroundColor =  UIColor .lightGray
-           
-           let lbl = UILabel(frame: CGRect(x: 15, y: 0, width: view.frame.width - 15, height: 40))
+           view.backgroundColor =  UIColor .systemGray5
+        var imageView : UIImageView
+        imageView  = UIImageView(frame:CGRect(x: 3, y: 10, width: 30, height: 30));
+        imageView.backgroundColor = UIColor .lightGray
+        imageView.layer.cornerRadius = 15
+        view.addSubview(imageView)
+           let lbl = UILabel(frame: CGRect(x: 45, y: 0, width: view.frame.width - 15, height: 40))
            lbl.font = UIFont.boldSystemFont(ofSize: 15)
-           lbl.text = group[section].groupName
- 
-        let lblSub = UILabel(frame: CGRect(x: 16, y: 15, width: view.frame.width - 15, height: 40))
-        lblSub.font = UIFont.systemFont(ofSize: 10)
-        lblSub.text = group[section].members
+        //lbl.text = searching ? filteredGroup[section].groupName : group[section].groupName
+        lbl.text = filteredGroup[section].groupName
         
+        let lblSub = UILabel(frame: CGRect(x: 46, y: 15, width: view.frame.width - 15, height: 40))
+        lblSub.font = UIFont.systemFont(ofSize: 10)
+        //lblSub.text = searching ? filteredGroup[section].members : group[section].members
+        lblSub.text = filteredGroup[section].members
            view.addSubview(lbl)
         view.addSubview(lblSub)
            return view
          }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return group[section].typeName?.count ?? 0
+     
+        return (filteredGroup[section].typeName?.count) ?? 0
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = group[indexPath.section].typeName?[indexPath.row]
+        cell.textLabel?.text = filteredGroup[indexPath.section].typeName?[indexPath.row]
               return cell
     }
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -97,7 +104,8 @@ extension ViewController : UITableViewDataSource, UITableViewDelegate{
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchText.isEmpty {
            searching = false
-            filteredGroup.removeAll()
+           // filteredGroup.removeAll()
+            tblData.reloadData()
         } else {
            searching = true
             filteredGroup = group.filter{$0.groupName?.range(of: searchText, options: .caseInsensitive) != nil}
@@ -110,6 +118,7 @@ extension ViewController : UITableViewDataSource, UITableViewDelegate{
     }
     
 }
+
 extension UIViewController {
     func hideKeyboardWhenTappedAround() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
